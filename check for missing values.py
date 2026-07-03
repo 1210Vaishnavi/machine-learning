788149b3f8ba74e455378049c9802002e6b5cae9
THAMMINENI VAISHNAVI <thamminenivaishnavi1210@gmail.com>
2026-07-03 15:59:33 +0530

    Add missing values summary to the script

--- check for missing values.py
+++ check for missing values.py
@@ -7,3 +7,19 @@ print(titanic.isnull().sum())
 sns.heatmap(titanic.isnull(),cbar=False,yticklabels=False,cmap='viridis')
 plt.title('Missing values Heatmap')
 plt.show()
+survived         0
+pclass           0
+sex              0
+age            177
+sibsp            0
+parch            0
+fare             0
+embarked         2
+class            0
+who              0
+adult_male       0
+deck           688
+embark_town      2
+alive            0
+alone            0
+dtype: int64
