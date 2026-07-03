38b9e1a90aaea6d50a2a60b55c3479211fc307ac
THAMMINENI VAISHNAVI <thamminenivaishnavi1210@gmail.com>
2026-07-03 14:54:43 +0530

    Fix import statement for numpy and seaborn

--- load the dataset.py
+++ load the dataset.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as np'import seaborn as sns
+import numpy as np import seaborn as sns
 import matplotlib.pyplot as plt
 titanic=sns.load_dataset('titanic')
 print(titanic.head())
