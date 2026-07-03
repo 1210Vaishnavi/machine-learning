b42df5deb9d952e4e5101182097b0d058673f2f0
THAMMINENI VAISHNAVI <thamminenivaishnavi1210@gmail.com>
2026-07-03 15:53:44 +0530

    Load and display Titanic dataset
    Added dataset loading and initial print of the Titanic dataset.

--- load the dataset.py
+++ load the dataset.py
@@ -3,3 +3,16 @@ import numpy as np import seaborn as sns
 import matplotlib.pyplot as plt
 titanic=sns.load_dataset('titanic')
 print(titanic.head())
+survived  pclass     sex   age  sibsp  parch     fare embarked  class  \
+0         0       3    male  22.0      1      0   7.2500        S  Third   
+1         1       1  female  38.0      1      0  71.2833        C  First   
+2         1       3  female  26.0      0      0   7.9250        S  Third   
+3         1       1  female  35.0      1      0  53.1000        S  First   
+4         0       3    male  35.0      0      0   8.0500        S  Third   
+
+     who  adult_male deck  embark_town alive  alone  
+0    man        True  NaN  Southampton    no  False  
+1  woman       False    C    Cherbourg   yes  False  
+2  woman       False  NaN  Southampton   yes   True  
+3  woman       False    C  Southampton   yes  False  
+4    man        True  NaN  Southampton    no   True  
